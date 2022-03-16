55e679b29fbe5a51114b20bcde2ce2ca860d64b7
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2022-03-16 09:33:56 +0100

    Delete 2_Calc-f4.sh

--- 2_Calc-f4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script to run Calc-f4
-
-# Take into account that previously you must have collected all input filenames into a filelist
-
-# Run it in 6 threads and with 32 samples
-
-Calc-f4 -i pmau_all.filelist -n 32 -t 6 | gzip -f > pmau.100.minmac2.calcf4.f4.gz
