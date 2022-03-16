41641b307585313c0f7e2450f8cd5d37760ecb46
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2022-03-16 10:33:54 +0100

    Update 2_treemix.sh

--- TreeMix/2_treemix.sh
+++ TreeMix/2_treemix.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
+
 # Script to run treemix
 
 MASTER=/users-d3/jferrer/gizquierdo/TFG/treemix
 input_file=$MASTER/input_files/pmau_treemix.LDpruned.treemix.frq.gz
 output=pmau_treemix
 
-# Options: -m specifies the number of admixture events; -o output; -bootstrap -k the number of bootstraps; -noss (not applied) to make correction for few inds
+# Options: -m "INT" --> specifies the number of admixture events
+#          -o "STRING" --> output
+#          -bootstrap -k "INT" --> the number of bootstraps
+#          -noss --> to avoid making correction for few inds (not applied)
 
 for i in {0..10}
 	do
 		treemix -i $input_file -m $i -o $MASTER/bootstrap_1000_output/$output.$i -root P.puffinus -bootstrap -k 1000 > $MASTER/bootstrap_1000_output/$output.$i.log
 	done
 
-# Now we repeat the process with -noss (which shouldn't change anything as the only thing it does is correct when there are branches with length 0
+# Now we repeat the process with -noss (which shouldn't change much the output)
 
 for i in {0..10}
         do
