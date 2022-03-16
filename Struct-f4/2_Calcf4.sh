8c6584abaaa21b00cb827cc02a68318a0865f43e
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2022-03-16 09:37:53 +0100

    Create 2_Calcf4.sh

---
+++ Struct-f4/2_Calcf4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Script to run Calc-f4; this script has to be placed inside the same direct
+# Previously collect all input filenames into a filelist (with the format "path/filename\n")
+
+# Options: -i "STRING" --> filelist name
+#          -n "INT" --> nº of individuals included
+#          -t "INT" --> nº of threads to run it on
+
+Calc-f4 -i pmau_all.filelist -n 32 -t 6 | gzip -f > pmau.100.minmac2.calcf4.f4.gz
