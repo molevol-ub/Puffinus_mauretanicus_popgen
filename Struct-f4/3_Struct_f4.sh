303a5aa648612c58b55e11232fb4e5a5b07587b5
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2022-03-31 08:48:40 +0200

    Create 3_Struct_f4.sh

---
+++ Struct-f4/3_Struct_f4.sh
@@ -0,0 +1,16 @@
+#!/soft/R-4.1.1/bin/Rscript
+
+# Rscript to run Struct-f4 according to https://bitbucket.org/plibradosanz/structf4/src/master/
+
+# Send to either hercules11 or hercules 12
+
+# Options: -f "CHAR" --> path to input file
+#          -T "INT" --> number of threads
+#          -K "INT" --> number of ancestral populations
+#          -p "CHAR" --> path to plots (pdf) output
+
+Struct-f4 -f /path/file -T 6 -K 2 -p /output-path > /path/output_summary.out
+
+# Additionally you may choose to use: -m "INT" --> number of iterations for 1st MCMC chain
+#                                     -n "INT" --> number of iterations for 2nd MCMC chain
+# You can reduce them from the default settings (m=1M & n=5M) but first check their efficiency with the output plots with the default settings
