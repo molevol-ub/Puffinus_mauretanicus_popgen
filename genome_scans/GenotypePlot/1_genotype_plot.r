fc8b2ae9671180a1be174277de0516333370a944
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2022-07-15 08:42:40 +0200

    Update 1_genotype_plot.r

--- genome_scans/GenotypePlot/1_genotype_plot.r
+++ genome_scans/GenotypePlot/1_genotype_plot.r
@@ -1,21 +1,26 @@
 #!/soft/R-4.1.1
 
 # Rscript to generate 2 genotype plots: one with P.puffinus, one without; based on https://github.com/JimWhiting91/genotype_plot
+# You might want to consider: a) Varying missingness to include the max # SNPs and b) Set a high mac filter - this will reduce low-freq variants, which might be better for haplotype visualization purposes, but you might loose information.
 
 library(GenotypePlot)
 library(vcfR)
 library(ggplot2)
 
-# Preparing input files: aside from a population map with 2 columns, a VCF file of the desired region has to be compressed (bgzip) and indexed (tabix) before reading it
+# 1. Preparing input files: aside from a population map with 2 columns, a VCF file of the desired region has to be compressed (bgzip) and indexed (tabix) before reading it
 
-setwd("/home/guillem/Documents/TFG/genome_scans/genotype_plot/scf7180000013317/wPP")
+setwd("/your/directory")
 
-# Read in the subsetted VCF
+# 2. Read in the subsetted VCF as well as the "population map"
 
 our_popmap<-as.data.frame(read.table("../../Puffinus.list", h=F, sep=",", stringsAsFactors = FALSE))
-our_vcf <- read.vcfR("scf2.map.recode.vcf.gz")
+our_vcf <- read.vcfR("your_file.vcf.gz")
 
-# Generate plot and save as png; you can only do 1 scaffold at a time!
+# 3. Generate plot and save as png; you can only do 1 scaffold at a time!
+
+# Options:	cluster --> whether or not to cluster haplotypes for similarity
+#		snp_label_size --> interval between position labels
+#		polarise_genotypes --> IMPORTANT! You may polarize for: a) The ancestral population (outgroup) - but this might be less efficient if that one is heterozygous - or b) Either of the most dissimilar populations (better for visualization purposes)
 
 plot_scf1 <- genotype_plot(vcf_object=our_vcf, popmap=our_popmap, cluster =FALSE, snp_label_size=100000, 
 	colour_scheme=c("#7A0001","#d56d5d", "#789FF2"), invariant_filter=TRUE, polarise_genotypes="Pitiuses")
@@ -24,7 +29,7 @@ pdf("scf2_map.def.pdf", width=10, height=5)
 combine_genotype_plot(plot_scf1)
 dev.off()
 
-# Try also clustering individuals
+# 4. Try also clustering individuals
 
 plot_scf1 <- genotype_plot(vcf_object=our_vcf, popmap=our_popmap, cluster =TRUE, snp_label_size=50000, 
 	colour_scheme=c("#7A0001","#d56d5d", "#789FF2"), invariant_filter=TRUE, polarise_genotypes="Pitiuses")
@@ -37,7 +42,7 @@ pdf("scf12.def.pdf", width=14, height=7)
 combine_genotype_plot(plot_scf1)
 dev.off()
 
-# Finally, also do allele frequencies
+# 5. Finally, also do allele frequencies (plot_allele_frequency=TRUE)
 
 plot_scf1 <- genotype_plot(vcf_object=our_vcf, popmap=our_popmap, cluster =FALSE, snp_label_size=50000, 
 	colour_scheme=c("#7A0001","#d56d5d", "#789FF2"), invariant_filter=TRUE, plot_allele_frequency=TRUE, polarise_genotypes="Pitiuses")
