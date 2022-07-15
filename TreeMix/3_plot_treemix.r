a73711070d24a8adb6107c32d8ee431d1a14eb63
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2022-07-15 09:05:34 +0200

    Update 3_plot_treemix.r

--- TreeMix/3_plot_treemix.r
+++ TreeMix/3_plot_treemix.r
@@ -1,6 +1,6 @@
 #!/soft/R-4.1.1
 
-# R script to plot the results of the treemix analysis
+# R script to plot the results of the TreeMix analysis, as well as to tun OptM
 # Required packages are: RColorBrewer & R.utils
 # It also requires a script ("plotting_funcs.R") whose path you should specify
 
@@ -8,24 +8,65 @@ library(RColorBrewer)
 library(R.utils)
 source("../plotting_funcs.R")
 
-setwd("/home/guillem/Documents/TFG/treemix/bootstrap_1000_output")
-prefix="pmau_treemix"
+setwd("/your/dir")
+prefix="your_file_prefix"
 
-# Now you can run the script for every "edge" you want
-
-#par(mfrow=c(2,3))
+# 1. Now you can run the script to plot for every "edge" you want; you also check the % variance explained (csv output)
 
 for(edge in 0:10){
-	plot_tree(cex=0.8,paste0(prefix,".",edge))
+	file_name = paste("pmau_treemix.",edge,".main.png", sep="")
+	png(file_name, width=1300, height=650, res=150)
+	plot_tree(cex=1,paste0(prefix,".",edge, ".main"), mbar = F, arrow=0.1, ybar=0.55, plus=0.03)
 	title(paste(edge,"edges"))
-	file_name = paste("pmau_treemix_plot",edge,".png", sep="")
-	png(file_name, width=1500, height=700, res=150)
+	dev.off()
+
+	var_val<-get_f(stem=paste0(prefix,".",edge, ".main"))
+	output<-as.data.frame(var_val)
+	table_name=paste("explained_variance.",edge,".csv")
+	write.table(output, table_name)
+	
 }
 
-# This last bit is to check which parts of the tree are not well modelled for the different runs
+# 2. This bit is to check which parts of the tree are not well modelled for the different runs (plot the residuals)
 
 for(edge in 0:10){
-	plot_resid(stem=paste0(prefix,".",edge),pop_order="../pmau_pops.list")
-	file_name = paste("pmau_residuals_plot",edge,".png", sep="")
-	png(file_name, width=1500, height=700, res=150)
+
+	file_name = paste("pmau_residuals.main.",edge,".png", sep="")
+	png(file_name, width=1500, height=1100, res=150)
+	plot_resid(stem=paste0(prefix,".",edge, ".main"),pop_order="../pmau_pops.list")
+	dev.off()
 }
+
+
+#-----------------------------------------------------------------------------------------------------------
+
+# You can use OptM to check which TreeMix graph is most viable using the 3 algorithms proposed ("Evanno", "linear" and "SiZer")
+
+setwd("/home/guillem/Documents/TFG/treemix_v2")
+
+library("OptM")
+
+# 3.1. "Evanno"-like method
+
+evanno.optM=optM("/home/guillem/Documents/TFG/treemix_v2/subgroups_output_3/OptM")
+
+plot_optM(evanno.optM, method = "Evanno", pdf="./subgroups_output_3/Evanno-OptM.pdf")
+dev.off()
+
+# 3.2. Linear modeling estimates method
+
+linear.optM=optM("/home/guillem/Documents/TFG/treemix_v2/subgroups_output_3/OptM", method = "linear")
+
+file_name = paste("./subgroups_output_3/Linear-OptM.png", sep="")
+png(file_name, width=1500, height=950, res=150)
+plot_optM(linear.optM, method = "linear")
+dev.off()
+
+# 3.3. SiZer method
+
+SiZer.optM=optM("/home/guillem/Documents/TFG/treemix_v2/subgroups_output_3/OptM", method = "SiZer")
+
+file_name = paste("./subgroups_output_3/SiZer-OptM.png", sep="")
+png(file_name, width=1500, height=950, res=150)
+plot_optM(SiZer.optM, method = "SiZer")
+dev.off()
