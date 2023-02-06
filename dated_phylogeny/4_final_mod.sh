65d1c5e9520b1ca892fa6762920a156717dddcab
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-02-06 10:52:32 +0100

    Update and rename 5_final_mod.sh to 4_final_mod.sh

---
+++ dated_phylogeny/4_final_mod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -e fixed_diff.err
+#$ -o fixed_diff.out
+#$ -q h13.q
+#$ -pe ompi255h13 4
+#$ -V                    #export environment var
+#$ -N fixed_diff            #name Job
+#$ -M 000izquierdoguillem@gmail.com
+#$ -m be
+
+# Depending on your beast version, you might have to change the following utility:
+
+for file in ./*.xml;
+do;
+sed 's/beast.util.TreeParser/beast.base.evolution.tree.TreeParser/g' $file > $file.def;
+mv $file.def $file;
+done
--- dated_phylogeny/5_final_mod.sh
+++
@@ -1,7 +0,0 @@
-# You have to change the following utility:
-
-for file in ./*.xml;
-do;
-sed 's/beast.util.TreeParser/beast.base.evolution.tree.TreeParser/g' $file > $file.def;
-mv $file.def $file;
-done
