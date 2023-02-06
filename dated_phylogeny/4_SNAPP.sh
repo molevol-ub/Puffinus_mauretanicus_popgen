b8dd7ec8978f66e007455e15eb051dfb79858056
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-02-06 10:40:44 +0100

    Update and rename 2_SNAPP.sh to 4_SNAPP.sh

--- dated_phylogeny/4_SNAPP.sh
+++ dated_phylogeny/4_SNAPP.sh
@@ -10,6 +10,10 @@
 #$ -M 000izquierdoguillem@gmail.com
 #$ -m be
 
+# Simple script to run SNAPP
+
+# Take into account that the SNAPP input .xml files have to previously be prepared, ideally using the scripts foudn in this directory
+
 WORKDIR=/users-d3/jferrer/gizquierdo/TFM/dated_phylo/SNAPP/maxmiss80/scfE
 
 /users-d3/jferrer/gizquierdo/beast/bin/beast -threads 24 $WORKDIR/scfE_downsized.maxmiss80.phased.xml
