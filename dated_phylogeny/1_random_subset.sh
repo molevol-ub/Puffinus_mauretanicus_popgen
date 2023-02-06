8d30c87240ce76eb0f30a1d13df97f9b9f985430
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-02-06 10:37:24 +0100

    Update 1_random_subset.sh

--- dated_phylogeny/1_random_subset.sh
+++ dated_phylogeny/1_random_subset.sh
@@ -10,21 +10,23 @@
 #$ -M 000izquierdoguillem@gmail.com
 #$ -m be
 
-#bcftools view Puffinus_downsized.wPP.maxmiss80.nomono.vcf.gz | vcfrandomsample -r 0.00012645 > Puffinus_downsized.wPP.maxmiss80.subset1.vcf
-#bcftools view Puffinus_downsized.wPP.maxmiss80.nomono.vcf.gz | vcfrandomsample -r 0.00012645 > Puffinus_downsized.wPP.maxmiss80.subset2.vcf
-#bcftools view Puffinus_downsized.wPP.maxmiss80.nomono.vcf.gz | vcfrandomsample -r 0.00012645 > Puffinus_downsized.wPP.maxmiss80.subset3.vcf
-#bcftools view Puffinus_downsized.wPP.maxmiss80.nomono.vcf.gz | vcfrandomsample -r 0.00012645 > Puffinus_downsized.wPP.maxmiss80.subset4.vcf
-#bcftools view Puffinus_downsized.wPP.maxmiss80.nomono.vcf.gz | vcfrandomsample -r 0.00012645 > Puffinus_downsized.wPP.maxmiss80.subset5.vcf
+# Script to prepare the input vcf for SNAPP
 
-#vcftools --gzvcf Puffinus_downsized.wPP.maxmiss80.nomono.vcf.gz --max-missing 1 --recode --out Puffinus_downsized.wPP.maxmiss100
-#mv Puffinus_downsized.wPP.maxmiss100.recode.vcf Puffinus_downsized.wPP.maxmiss100.nomono.vcf
+# 1. Make a vcf file with only representatative individuals of the populations/groups you want to include
 
-#bgzip Puffinus_downsized.wPP.maxmiss100.nomono.vcf
-#tabix Puffinus_downsized.wPP.maxmiss100.nomono.vcf.gz
+vcftools --gzvcf /path/to/your/main_file.vcf.gz --indv ind1 --indv ind2 --indv ind3 --recode --our dataset_downsized
 
-bcftools view Puffinus_downsized.wPP.maxmiss100.nomono.vcf.gz | vcfrandomsample -r 0.00093691 > Puffinus_downsized.wPP.maxmiss100.subset6.vcf 
-bcftools view Puffinus_downsized.wPP.maxmiss100.nomono.vcf.gz | vcfrandomsample -r 0.00093691 > Puffinus_downsized.wPP.maxmiss100.subset7.vcf
-bcftools view Puffinus_downsized.wPP.maxmiss100.nomono.vcf.gz | vcfrandomsample -r 0.00093691 > Puffinus_downsized.wPP.maxmiss100.subset8.vcf
-bcftools view Puffinus_downsized.wPP.maxmiss100.nomono.vcf.gz | vcfrandomsample -r 0.00093691 > Puffinus_downsized.wPP.maxmiss100.subset9.vcf
-bcftools view Puffinus_downsized.wPP.maxmiss100.nomono.vcf.gz | vcfrandomsample -r 0.00093691 > Puffinus_downsized.wPP.maxmiss100.subset10.vcf
+mv dataset_downsized.recode.vcf dataset_downsized.vcf
+bgzip dataset_downsized.vcf
+tabix dataset_downsized.vcf.gz
 
+# 2. Make a 5 random subsamples of your file with 1k datasets
+
+# -r --> ratio of the nº of snps you want to keep vs total. For ex.: if you want to keep 1k SNPs from a 1M SNP original dataset, then -r 0.001
+
+for i in {1..5}
+do
+
+bcftools view dataset_downsized.vcf.gz | vcfrandomsample -r 0.001 > dataset_downsized.vcf.gz.$i.vcf 
+
+done
