20f81c97e1ff649b2446fd271cfc3f55ec4552d5
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-02-06 10:51:24 +0100

    Update 4_snapp_preprb.sh

--- dated_phylogeny/4_snapp_preprb.sh
+++ dated_phylogeny/4_snapp_preprb.sh
@@ -1,3 +1,11 @@
-# Rscript to run snapp_prep.rb in order to prepare files for time-callibrated SNAPP analyses
+# Bash script to run snapp_prep.rb in order to prepare files for time-callibrated SNAPP analyses
 
-ruby snapp_prep-master/snapp_prep.rb -v scfE_downsized.maxmiss100.vcf -c constraints_taxa.normal.txt -t Puffinus_subset.csv -m 1000 -l 500000 -x scfE_downsized.maxmiss100.normal.xml -o scfE_downsized.maxmiss100.normal
+# Options:  -v [string] --> input vcf file
+#           -c [string] --> constraints file for the tree callibration --> see an example within this directory
+#           -t [string] --> file that relates populations and individuals
+#           -m [int] --> nº of iterations between logs
+#           -l [int] --> MCMC chain length
+#           -x, -o [string] --> name of the ouput
+
+ruby /path/to/snapp_prep.rb -v scfE_downsized.maxmiss100.vcf -c constraints_taxa.normal.txt -t Puffinus_subset.csv \
+-m 1000 -l 500000 -x scfE_downsized.maxmiss100.normal.xml -o scfE_downsized.maxmiss100.normal
