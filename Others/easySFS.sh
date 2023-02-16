b1e6eb4075289014c4402cc3fa529fdd5509c9be
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-02-16 10:41:47 +0100

    Update and rename 1_easySFS.sh to easySFS.sh

--- Others/easySFS.sh
+++ Others/easySFS.sh
@@ -8,14 +8,15 @@
 #$ -V                    #export environment var
 #$ -N easySFS             #name Job
 
+# Use easySFS.py to create folded SFS using a the projection into the x,y desired haplotype indvs (--proj).
+
 source activate whatshap
 export PATH=/users-d3/jferrer/gizquierdo/TFM/SFS/scripts:$PATH
 
 VCF=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss100.filtered.noPP.merged.nomono.masked.vcf.gz
-popfile=/users-d3/jferrer/gizquierdo/TFM/SFS/pops_file.txt
+popfile=/users-d3/jferrer/gizquierdo/TFM/SFS/pops_file.txt         # Popfile containing the indvs of each population in the vcf
 
-#easySFS.py -a -i $VCF -p $popfile --preview
+#easySFS.py -a -i $VCF -p $popfile --preview               # Use to choose best projection to use when dealing with missing data
 
 easySFS.py -a -i $VCF -p $popfile --proj 12,12 -o /users-d3/jferrer/gizquierdo/TFM/SFS/Pmau_auto_100.4 --prefix Pmau_Pyel
-#easySFS.py -a -i $VCF -p $popfile --proj 22,8 -o /users-d3/jferrer/gizquierdo/TFM/SFS/Z_65 --prefix Puffinus_SFS.Z.65
 
