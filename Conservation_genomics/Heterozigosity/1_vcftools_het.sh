45c814e8fcd479b7f4b26e9f4e458ba84c8b5030
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-02-21 10:38:14 +0100

    Update 1_vcftools_het.sh

--- Conservation_genomics/Heterozigosity/1_vcftools_het.sh
+++ Conservation_genomics/Heterozigosity/1_vcftools_het.sh
@@ -11,17 +11,6 @@
 cd /users-d3/jferrer/gizquierdo/TFM/conservation_genomics/het
 
 VCF=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss100.filtered.noPP.merged.nomono.masked.vcf.gz
-VCF2=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss100.filtered.wPP.merged.nomono.masked.vcf.gz
-VCF3=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss100.filtered.noPP.merged.nomono.minmac2.masked.vcf.gz
-VCF4=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss80.filtered.noPP.merged.nomono.masked.vcf.gz
-VCF5=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss80.filtered.noPP.merged.nomono.minmac2.masked.vcf.gz
-VCF6=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/chrZ/Puffinus_SNP.maxmiss65.filtered.noPP.merged.nomono.masked.vcf.gz
-VCF7=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/chrZ/Puffinus_SNP.maxmiss65.filtered.noPP.merged.nomono.minmac2.masked.vcf.gz
+outname=vcftools_het.noPP.maxmiss100
 
-vcftools --gzvcf $VCF --hardy --out vcftools_hardy.noPP.maxmiss100
-vcftools --gzvcf $VCF2 --hardy --out vcftools_hardy.wPP.maxmiss100 
-vcftools --gzvcf $VCF3 --hardy --out vcftools_hardy.noPP.maxmiss100.minmac2
-vcftools --gzvcf $VCF4 --hardy --out vcftools_hardy.noPP.maxmiss80
-vcftools --gzvcf $VCF5 --hardy --out vcftools_hardy.noPP.maxmiss80.minmac2
-vcftools --gzvcf $VCF6 --hardy --out vcftools_hardy.chrZ.noPP.maxmiss65
-vcftools --gzvcf $VCF7 --hardy --out vcftools_hardy.chrZ.noPP.maxmiss65.minmac2
+vcftools --gzvcf $VCF --het --out $outname
