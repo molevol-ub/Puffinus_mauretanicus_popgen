902dce085916840afa16bdb29594d608cae85ce1
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-11 16:40:49 +0200

    Create 1_allignment_cactus.sh

---
+++ Conservation_genomics/Cactus/1_allignment_cactus.sh
@@ -0,0 +1,7 @@
+# Enter the conda environment where you have installed Cactus (in this case with Python version 3.9)
+
+source activate py39
+
+# Remember to run with Singularity instead of Docker
+
+--binariesMode singularity
