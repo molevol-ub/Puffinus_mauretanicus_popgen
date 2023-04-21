24e003d31699e556471f2457d817efab2d42db75
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-21 17:27:44 +0200

    Update 1_prepare_input.sh

--- Population_structure/Chromopainter/1_prepare_input.sh
+++ Population_structure/Chromopainter/1_prepare_input.sh
@@ -1,9 +1,24 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -e gatk_reference.err
+#$ -o gatk_reference.out
+#$ -q h12.q
+#$ -pe ompi128h12 4
+#$ -V                    #export environment var
+#$ -N gatk_reference             #name Job
+#$ -M 000izquierdoguillem@gmail.com
+#$ -m be
+
 
 workdir=/users-d3/jferrer/gizquierdo/TFM/chromopainter
 cd $workdir
 
-# 1. First create IMPUTE file from phased VCFs
+# 1. Transform VCF to chromopainter input (if it works)
 
 invcf=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80.vcf.gz
+invcf_nomono=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80.vcf
 
-vcftools --gzvcf $invcf --IMPUTE --out Puffinus.auto.nPP.maxmiss80
+bcftools view -Ov -o $invcf_nomono --threads 4 -e 'COUNT(GT="AA")=N_SAMPLES || COUNT(GT="RR")=N_SAMPLES' $invcf
+
+vcf_to_chromopainter_main.R -g $invcf_nomono -o Puffinus.auto.nPP.maxmiss80
