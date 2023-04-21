dd2b62f5c93f4cb778c64bde8e0c5baae67f4780
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-21 13:45:29 +0200

    Update 1_prepare_input.sh

--- Population_structure/Chromopainter/1_prepare_input.sh
+++ Population_structure/Chromopainter/1_prepare_input.sh
@@ -1,1 +1,9 @@
 
+workdir=/users-d3/jferrer/gizquierdo/TFM/chromopainter
+cd $workdir
+
+# 1. First create IMPUTE file from phased VCFs
+
+invcf=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80.vcf.gz
+
+vcftools --gzvcf $invcf --IMPUTE --out Puffinus.auto.nPP.maxmiss80
