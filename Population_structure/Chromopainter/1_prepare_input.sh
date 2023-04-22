8f84126bd0a5074efb9562dbbeb3bc910b57bfcd
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-22 19:47:58 +0200

    Update 1_prepare_input.sh

--- Population_structure/Chromopainter/1_prepare_input.sh
+++ Population_structure/Chromopainter/1_prepare_input.sh
@@ -21,10 +21,12 @@ invcf=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/P
 outvcf=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80
 outvcf_nomono=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80.vcf
 
-vcftools --gzvcf $invcf --max-missing 0.8 --min-alleles 2 --max-alleles 2 --minDP 4 --maxDP 50 --minQ 30 --remove-indv M22 --remove-indv sacella
---remove-indv M7 --remove-indv COP1 --remove-indv LT2 --remove-filtered-all --recode-INFO-all --recode --out 
+vcftools --gzvcf $invcf --max-missing 0.8 --min-alleles 2 --max-alleles 2 --minDP 4 --maxDP 50 --minQ 30 --remove-indv M22 --remove-indv sacella \
+--remove-indv M7 --remove-indv COP1 --remove-indv LT2 --remove-filtered-all --recode-INFO-all --recode --out $outvcf
 mv $outvcf.recode.vcf $outvcf.vcf
 
 bcftools view -Ov -o $outvcf_nomono --threads 4 -e 'COUNT(GT="AA")=N_SAMPLES || COUNT(GT="RR")=N_SAMPLES' $outvcf.vcf
+
+
 
 vcf_to_chromopainter_main.R -g $outvcf_nomono -o Puffinus.auto.nPP.maxmiss80
