1a0083669cd1c06652d2d2381ef5767f0970baa5
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-23 22:13:01 +0200

    Update 2_run_chromopainter.sh

--- Population_structure/Chromopainter/2_run_chromopainter.sh
+++ Population_structure/Chromopainter/2_run_chromopainter.sh
@@ -10,6 +10,8 @@
 #$ -M 000izquierdoguillem@gmail.com
 #$ -m be
 
+cd /users-d3/jferrer/gizquierdo/TFM/chromopainter
+
 # 1. Estimate the required parameters fro chromopainter using a SUBSAMPLE OF INDIVIDUALS AND SITES; use options:
 #      · “-s 0” to specify that no paintingsamples are needed in this step
 #      · “-i 10 -in -iM” specifies to use 10 iterations of Expectation-Maximisation to infer the switch (“-in”) and emission (“-iM”) parameters. 
@@ -19,3 +21,6 @@ ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.aut
 # 2. Calculate the average estimated values for n and M to use in the final chromopainter run. The script might need some polishing to use the appropiate input (parameters "$infilePREFIX", "$infileSUFFIX", "@chromovec" and "@chromolengths")
 # For more info see: https://github.com/hellenthal-group-UCL/fastGLOBETROTTER/blob/master/tutorial/ChromoPainterv2EstimatedNeMutExtractEM.pl
 
+perl scripts/ChromoPainterv2EstimatedNeMutExtractEM.pl 
+
+# 3. Using the output values of the previous step, run chromopainter
