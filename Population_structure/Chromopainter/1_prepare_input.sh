4719b6ce053d8ec5266aac53f22b4ef7661ebf82
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-23 20:22:17 +0200

    Update 1_prepare_input.sh

--- Population_structure/Chromopainter/1_prepare_input.sh
+++ Population_structure/Chromopainter/1_prepare_input.sh
@@ -14,7 +14,7 @@
 workdir=/users-d3/jferrer/gizquierdo/TFM/chromopainter
 cd $workdir
 
-# 1. Transform VCF to chromopainter input (if it works) using the script provided in: https://github.com/sahwa/vcf_to_chromopainter
+# 1. Prepare your VCFs with the desired SNPs
 # You must keep the INFO field of shapeit4-phased VCFs to allow the inference of recombination rates
 
 invcf=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_raw.auto.shapeit4_whatshap_phased.vcf.gz
@@ -30,6 +30,13 @@ mv prova $outvcf.vcf
 
 bcftools view -Ov -o $outvcf_nomono --threads 4 -e 'COUNT(GT="AA")=N_SAMPLES || COUNT(GT="RR")=N_SAMPLES' $outvcf.vcf
 
-
+# 2. Transform VCF to chromopainter input (if it works) using the script provided in: https://github.com/sahwa/vcf_to_chromopainter
 
 vcf_to_chromopainter_main.R -g $outvcf_nomono -o Puffinus.auto.nPP.maxmiss80
+
+# 3. Remove spaces from the "haplotype" lines in the haplotype input file
+
+head Puffinus.auto.nPP.maxmiss80.chromopainter.inp -n 3 > prova
+tail -n +4 Puffinus.auto.nPP.maxmiss80.chromopainter.inp | sed 's/ //g' >> prova
+mv prova Puffinus.auto.nPP.maxmiss80.chromopainter.inp
+
