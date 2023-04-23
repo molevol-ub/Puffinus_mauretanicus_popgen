81321e69c858479f799f19809bcd8f8718d11c83
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-23 20:39:01 +0200

    Update 2_run_chromopainter.sh

--- Population_structure/Chromopainter/2_run_chromopainter.sh
+++ Population_structure/Chromopainter/2_run_chromopainter.sh
@@ -14,4 +14,4 @@
 #      · “-s 0” to specify that no paintingsamples are needed in this step
 #      · “-i 10 -in -iM” specifies to use 10 iterations of Expectation-Maximisation to infer the switch (“-in”) and emission (“-iM”) parameters. 
 
-ChromoPainterv2 -g prova.txt -r Puffinus.auto.nPP.maxmiss80.recomrates.txt -a 0 0 -s 0 -i 10 -in -iM -o Puffinus.auto.nPP.maxmiss80
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt -a 0 0 -s 0 -i 10 -in -iM -o Puffinus.auto.nPP.maxmiss80
