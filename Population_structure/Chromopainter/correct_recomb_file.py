76e3960abb9fc962362acd665ff2bcca5f5937f5
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-24 16:32:41 +0200

    Create correct_recomb_file.py

---
+++ Population_structure/Chromopainter/correct_recomb_file.py
@@ -0,0 +1,34 @@
+# Python script to add separation between chromosomes in the recombination input file (add -9 to the last position of each chromosome)
+
+import re
+infile=open("/users-d3/jferrer/gizquierdo/TFM/chromopainter/Puffinus_subset.recomrates.txt")
+outfile=open("/users-d3/jferrer/gizquierdo/TFM/chromopainter/prova.txt", "w")
+
+count=0
+first_line=""
+first_recomb="0.00000001"
+
+for line in infile:
+  if "start" not in line:
+    exp=re.search(r"(.+) (.+)\n", line)
+    pos=int(exp.group(1))
+    recomb=exp.group(2)
+    
+    if pos > count:                                                  # If the current position is smaller than the preivous one...
+      if count != 0:
+        outfile.write(str(count) + " " + first_recomb + "\n")
+    else:
+      outfile.write(str(count) + " -9\n")                             # ... add the -9 to the previous position
+    
+    count=pos
+    first_line=line
+    first_recomb=recomb
+    
+  else:
+    outfile.write(line)           # Write the header
+    
+
+infile.close()
+outfile.close()
+
+# This script does not consider the last line of the recombination file, which must be copied manually
