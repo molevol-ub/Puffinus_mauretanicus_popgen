3dc675e467f16822e177865c2b8e8478ca843466
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-25 09:55:30 +0200

    Update 2_hal2fasta.sh

--- Conservation_genomics/Cactus/2_hal2fasta.sh
+++ Conservation_genomics/Cactus/2_hal2fasta.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+# Run locally, hal2fasta does not work on hercules (missing libraries)
+
 #---------------------------------------------------------------------
 #---------------PREPARE  THE  CACTUS  363-WAY  ALLIGNMENT------------
 #---------------------------------------------------------------------
@@ -7,12 +11,16 @@
 wget -P /media/guillem/BC90A1CD90A18F08/Guillem/TFM_GIA/Cactus https://cgl.gi.ucsc.edu/data/cactus/363-avian-2020-hub/Gallus_gallus/Gallus_gallus.maf.gz
 cd /your/dir
 
+# If needed install hal: instructions in https://github.com/ComparativeGenomicsToolkit/hal
 # 2. Export the library paths that are required in the following scripts; if you don't know the path, use "locate"
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/disk3/h-user/jferrer/programari/anaconda3/bin/cactus/js/build-temp-027501980/rootfs/usr/lib/x86_64-linux-gnu/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/disk3/h-user/vadim.pisarenco/anaconda3/lib/
+export PATH=<path to hal>/bin:${PATH}                    #/home/guillem/Documents/software/hal
+export PYTHONPATH=<parent of hal>:${PYTHONPATH}          #/home/guillem/Documents/software
 
-# 3. Convert HAL to FASTA using 
+# 3. Convert HAL to FASTA using hal2fasta
+
+bird363=/media/guillem/BC90A1CD90A18F08/Guillem/TFM_GIA/Cactus/363-avian-2020.hal
+hal2fasta $bird363 $(halStats --root $bird363) --subtree --upper --ucscSequenceNames > 363_bird.fa &
 
 
 # Remember to run with Singularity instead of Docker
