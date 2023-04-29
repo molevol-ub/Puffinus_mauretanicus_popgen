e2c8e2691c7390292a1cab3cb017cbccf513336c
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-29 12:46:47 +0200

    Update 3_run_cactus.sh

--- Conservation_genomics/Cactus/3_run_cactus.sh
+++ Conservation_genomics/Cactus/3_run_cactus.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -e gatk_reference.err
+#$ -o gatk_reference.out
+#$ -q h13.q
+#$ -pe ompi255h13 36
+#$ -V                    #export environment var
+#$ -N gatk_reference             #name Job
+#$ -M 000izquierdoguillem@gmail.com
+#$ -m be
+
 # We add our genomes to the 363-way allignment by solving 2 subproblems
 # More info here
 
