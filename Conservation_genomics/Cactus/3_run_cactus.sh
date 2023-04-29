33aa569bddad393d5ebd8448b40335cb56a0beeb
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-04-29 12:20:44 +0200

    Update 3_run_cactus.sh

--- Conservation_genomics/Cactus/3_run_cactus.sh
+++ Conservation_genomics/Cactus/3_run_cactus.sh
@@ -1,6 +1,7 @@
 # We add our genomes to the 363-way allignment by solving 2 subproblems
 # More info here
 
+source activate py39
 cd /users-d3/jferrer/gizquierdo/TFM/Cactus/Cactus
 
 cactus jobStore Puffinus_run1.txt Puffinus_run1.hal --root genome7 
