ead9498cf0a60bc9e8fbb4dc76c74af404ae54fa
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-05-01 13:11:48 +0200

    Update 2_run_chromopainter.sh

--- Population_structure/Chromopainter/2_run_chromopainter.sh
+++ Population_structure/Chromopainter/2_run_chromopainter.sh
@@ -28,3 +28,42 @@ perl scripts/ChromoPainterv2EstimatedNeMutExtractEM.pl
 # 3. Using the output values of the previous step, run chromopainter
 
 ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt -a 0 0 -n 33700.840 -M 0.022739 -o Puffinus.auto.nPP.maxmiss80
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+# 4. Prepare the input files for GLOBETROTTER
+
+# 4.1 Generate the copy vector input file
+
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f Puffinus.auto.nPP.maxmiss80.popfile.txt 0 0 -s 0 -n 33700.840 -M 0.022739 -o Puffinus.auto.nPP.maxmiss80
+
+# Then paint: the different iterations correspond to different "target" (coinciding with the directory name) and source populations
+
+mkdir Pyel
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_pyel.txt 0 0 -n 33700.840 -M 0.022739 -o ./Pyel/Puffinus.auto.nPP.maxmiss80
+mkdir Menorca
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_men.txt 0 0 -n 33700.840 -M 0.022739 -o ./Menorca/Puffinus.auto.nPP.maxmiss80
+mkdir Menorca_noMen
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_men2.txt 0 0 -n 33700.840 -M 0.022739 -o ./Menorca_noMen/Puffinus.auto.nPP.maxmiss80
+mkdir Menorca_noMenCab
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_men3.txt 0 0 -n 33700.840 -M 0.022739 -o ./Menorca_noMenCab/Puffinus.auto.nPP.maxmiss80
+mkdir Cabrera
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_cab.txt 0 0 -n 33700.840 -M 0.022739 -o ./Cabrera/Puffinus.auto.nPP.maxmiss80
+mkdir Cabrera_noCab
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_cab2.txt 0 0 -n 33700.840 -M 0.022739 -o ./Cabrera_noCab/Puffinus.auto.nPP.maxmiss80
+mkdir Cabrera_noMenCab
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_cab3.txt 0 0 -n 33700.840 -M 0.022739 -o ./Cabrera_noMenCab/Puffinus.auto.nPP.maxmiss80
+mkdir Mallorca
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_mal.txt 0 0 -n 33700.840 -M 0.022739 -o ./Mallorca/Puffinus.auto.nPP.maxmiss80
+mkdir Pitiuses
+ChromoPainterv2 -g Puffinus.auto.nPP.maxmiss80.chromopainter.inp -r Puffinus.auto.nPP.maxmiss80.recomrates.txt \
+  -t Puffinus.auto.nPP.maxmiss80.idfile.txt -f popfiles/Puffinus.auto.nPP.maxmiss80.popfile_pit.txt 0 0 -n 33700.840 -M 0.022739 -o ./Pitiuses/Puffinus.auto.nPP.maxmiss80
