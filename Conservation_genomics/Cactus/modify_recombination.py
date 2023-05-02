f2be8402ec39666acc18d40cbbcaf49b17146aa7
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-05-02 12:28:53 +0200

    Update modify_recombination.py

--- Conservation_genomics/Cactus/modify_recombination.py
+++ Conservation_genomics/Cactus/modify_recombination.py
@@ -4,31 +4,31 @@ infile=open("Puffinus.auto.nPP.maxmiss80.recomrates.txt")
 outfile=open("Puffinus.auto.nPP.maxmiss80.recomrates_positive.txt","w")
 
 last_pos=0
-last_pos_chr=0
+last_pos_chr=1
 count_position=0
 
 for line in infile:
   if "start.pos" in line:
     outfile.write(line)
   else:
-    exp=re.search(r"(.+)\t(.+)\n", line)
-    pos=int(exp.group(1))
-    rec=int(exp.group(2))
+    exp=re.search(r"(.+) (.+)\n", line)
+    pos=float(exp.group(1))
+    rec=exp.group(2)
     
     
     if "-9" in line:
-      outfile.write(str(pos) + "\t0.00000001\n")
+      outfile.write(str(pos) + " 0.00000001\n")
       last_pos_chr=pos
       last_pos=pos
     
     else:
       if last_pos==last_pos_chr:
         last_pos=pos+1000000                                            # After a change of chromosome, add 1 million bp to the previous position (last SNP of the previous chromosome)
-        outfile.write(str(last_pos) + "\t" + str(rec) + "\n")
+        outfile.write(str(last_pos) + " " + rec + "\n")
       
       else:
         last_pos+=pos                                                   # In the rest of lines sum to the previous one, which already has the milion bp incorporated
-        outfile.write(str(last_pos) + "\t" + str(rec) + "\n")
+        outfile.write(str(last_pos) + " " + rec + "\n")
       
 infile.close()
 outfile.close()
