2c57c90914982cf8b107ef2945508a5ab483fbc4
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-05-05 10:00:18 +0200

    Update 5_run_separated_scfs.sh

--- Population_structure/Chromopainter/5_run_separated_scfs.sh
+++ Population_structure/Chromopainter/5_run_separated_scfs.sh
@@ -3,15 +3,15 @@
 #$ -R y
 #$ -e divide_vcf.err
 #$ -o divide_vcf.out
-#$ -q h12.q
-#$ -pe ompi128h12 2
+#$ -q h11.q
+#$ -pe ompi64h11 1
 #$ -V                    #export environment var
 #$ -N divide_vcf             #name Job
-#$ -M 000izquierdoguillem@gmail.com
-#$ -m be
+#$ -t 1-876
+#$ -tc 24
 
 workdir=/users-d3/jferrer/gizquierdo/TFM/chromopainter/scaffolds
-mkdir $workdir
+#mkdir $workdir
 cd $workdir
 
 # 1. Divide your VCF by scaffolds
@@ -19,7 +19,10 @@ cd $workdir
 outvcf_nomono=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80.nomono.vcf
 scf_list=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/scf_list.txt
 
-while read line; do
-  vcftools --vcf $outvcf_nomono --chr $line --recode-INFO-all --recode --out $line
-  mv $line.recode.vcf $line.vcf
-done < $scf_list
+#while read line; do
+#  vcftools --vcf $outvcf_nomono --chr $line --recode-INFO-all --recode --out $line
+#  mv $line.recode.vcf $line.vcf
+#done < $scf_list
+
+vcftools --vcf $outvcf_nomono --chr ${scf[(($SGE_TASK_ID))]} --recode-INFO-all --recode --out ${scf[(($SGE_TASK_ID))]}
+mv ${scf[(($SGE_TASK_ID))]}\.recode.vcf ${scf[(($SGE_TASK_ID))]}\.vcf
