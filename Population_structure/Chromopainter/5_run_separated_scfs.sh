f80d11b92b9243c35336e4acce21c6a0fd24524c
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-05-05 10:05:55 +0200

    Update 5_run_separated_scfs.sh

--- Population_structure/Chromopainter/5_run_separated_scfs.sh
+++ Population_structure/Chromopainter/5_run_separated_scfs.sh
@@ -17,7 +17,7 @@ cd $workdir
 # 0. Make your list of scaffolds
 
 outvcf_nomono=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/phased_vcfs_def/autosomes/Puffinus_def.auto.shapeit4_whatshap_phased.nPP.maxmiss80.nomono.vcf
-zcat $outvcf_nomono | grep -v "^#" | cut -f1 | sort | uniq > scaffolds.txt
+cat $outvcf_nomono | grep -v "^#" | cut -f1 | sort | uniq > scaffolds.txt
 scf=($(cat scaffolds.txt))
 
 # 1. Divide your VCF by scaffolds
