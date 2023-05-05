47fff417482bfe55c799140d81509ab8d5b5e276
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-05-05 10:22:40 +0200

    Update 5_run_separated_scfs.sh

--- Population_structure/Chromopainter/5_run_separated_scfs.sh
+++ Population_structure/Chromopainter/5_run_separated_scfs.sh
@@ -22,10 +22,30 @@ scf=($(cat scaffolds.txt))
 
 # 1. Divide your VCF by scaffolds
 
-#while read line; do
-#  vcftools --vcf $outvcf_nomono --chr $line --recode-INFO-all --recode --out $line
-#  mv $line.recode.vcf $line.vcf
-#done < $scf_list
-
 vcftools --vcf $outvcf_nomono --chr ${scf[(($SGE_TASK_ID))]} --recode-INFO-all --recode --out ${scf[(($SGE_TASK_ID))]}
 mv ${scf[(($SGE_TASK_ID))]}\.recode.vcf ${scf[(($SGE_TASK_ID))]}\.vcf
+
+# Now for every scaffold
+
+while read scf; do
+
+  # 2. Transform VCF to chromopainter input (if it works) using the script provided in: https://github.com/sahwa/vcf_to_chromopainter
+  
+  vcf_to_chromopainter_main.R -g $scf.vcf -o $scf
+  
+  # 3. Remove spaces from the "haplotype" lines in the haplotype input file
+  
+  head $scf.chromopainter.inp -n 3 > prova
+  tail -n +4 $scf.chromopainter.inp | sed 's/ //g' >> prova
+  mv prova $scf.chromopainter.inp
+  
+  # 4. Modify the input recombination file so that the last recombination of every gene is substituted by "-9" to accomodate the requirements of chromopainter
+  
+  python correct_recomb_file.py $scf.recomrates.txt prova.txt
+  
+  # Copy the last line, that is not included in the script
+  
+  tail -n 1 $scf.recomrates.txt >> prova.txt
+  mv prova.txt $scf.recomrates.txt
+
+done < scaffolds.txt
