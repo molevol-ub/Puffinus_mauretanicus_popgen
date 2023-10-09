4670fca434f8ad54addade6bf3dc9e98e93ac42e
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-10-09 13:32:11 +0200

    Update and rename 1_ADMIXTOOLS.sh to 1_Dsuite.sh

--- Population_structure/Dstats/1_ADMIXTOOLS.sh
+++
@@ -1,1 +0,0 @@
-export PATH=/users-d3/jferrer/programari/AdmixTools/src:$PATH
---
+++ Population_structure/Dstats/1_Dsuite.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#$ -cwd
+#$ -R y
+#$ -e Dsuite.err
+#$ -o Dsuite.out
+#$ -q h12.q
+#$ -pe ompi128h12 4
+#$ -V                    #export environment var
+#$ -N Dsuite             #name Job
+#$ -M 000izquierdoguillem@gmail.com
+#$ -m be
+
+WORKDIR=/users-d3/jferrer/gizquierdo/TFM/Dstats
+cd $WORKDIR
+VCF=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss80.filtered.wPP.merged.nomono.masked.vcf.gz
+VCF_mac=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs/auto/Puffinus_SNP.maxmiss80.filtered.noPP.merged.nomono.minmac2.masked.vcf.gz
+
+# 1. Calculate D-stats and f-stats using Dtrios specifying an imput tree where Cabrera is the outgroup to all other Pmau
+
+Dsuite Dtrios -n Puffinus.Cabrera_out -t Puffinus.Cabrera_out.nwk $VCF Puffinus_list.txt
+
+echo "1st run completed"
+
+# Repeat but with a tree where Cabrera is sister to Mallorca
+
+Dsuite Dtrios -n Puffinus.Pit_out -t Puffinus.Pit_out.nwk $VCF Puffinus_list.txt
+
+echo "2nd run completed"
+
+cut -f 2 Puffinus_list.txt | uniq > plot_order.txt
