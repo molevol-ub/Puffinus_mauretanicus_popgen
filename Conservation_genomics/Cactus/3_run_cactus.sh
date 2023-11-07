1093efaa3ffe26a6fc1fd3f766dc914eda0f0aed
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2023-11-07 10:46:13 +0100

    Update 3_run_cactus.sh

--- Conservation_genomics/Cactus/3_run_cactus.sh
+++ Conservation_genomics/Cactus/3_run_cactus.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
-#$ -cwd
-#$ -R y
-#$ -e cactus.err
-#$ -o cactus.out
-#$ -q h13.q
-#$ -pe ompi255h13 36
-#$ -V                    #export environment var
-#$ -N cactus             #name Job
 #$ -M 000izquierdoguillem@gmail.com
 #$ -m be
 
 # We add our genomes to the 363-way allignment by solving 2 subproblems
 # More info here
 
+date
+
+export PATH=$PATH:~/programari/cactus-bin-v2.5.1/bin
 source activate py39
 cd /users-d3/jferrer/gizquierdo/TFM/Cactus/Cactus
 
 # Remember to run with Singularity instead of Docker
 
-cactus jobstore Puffinus_run1.txt Puffinus_run1.hal --root birdAnc361 --binariesMode local --consCores 36 --maxCores 36 --batchSystem grid_engine --logFile Cactus.log --rotatingLogging
+mkdir LOGS
+mkdir WDIR
 
+cactus jobstore Puffinus_run1.txt Puffinus_run1.hal --root birdAnc361 \
+        --maxCores 10 \
+        --logFile Cactus.log \
+        --binariesMode local \
+        --writeLogs LOGS/ \
+        --workDir WDIR \
+        --disableCaching \
+        --defaultDisk 8G \
+        --maxDisk 50G \
+        --nodeStorage 20 \
+        --consCores 10 \
+        --defaultMemory 8G \
+        --maxLocalJobs 10 \
+        --maxServiceJobs 10 
+
+date
