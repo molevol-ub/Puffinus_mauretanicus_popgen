f29102bdd749b0aa5dde6ad4b97c8c3d54fee0b6
00izquierdoguillem <94355688+00izquierdoguillem@users.noreply.github.com>
2024-10-09 10:35:48 +0200

    Update 1_filter_chrZ.sh

--- SNP_calling/Variant_filtering/1_filter_chrZ.sh
+++ SNP_calling/Variant_filtering/1_filter_chrZ.sh
@@ -2,15 +2,17 @@
 #$ -cwd
 #$ -V
 
-# Script to extract only the selected chrZ scaffolds from the vcf file
+# Script to extract only the selected chrZ or autosomal scaffolds from the vcf file
 
 # 1 Define files and pathways
 
-VCF_file=/users-d3/jferrer/pmau_popgen/SNP_calling/vcfs/Puffinus_intersect_raw.vcf.gz
-guillem=/users-d3/jferrer/gizquierdo/TFG/chr_split/vcfs
+VCF_file=/set/your/path/Puffinus_intersect_raw.vcf.gz
+guillem=/set/your/path
 
 bed_file=$guillem/auto/auto.bed
+bed_file2=$guillem/auto/chrZ.bed
 
 # 2 Filter the desired scaffolds using vcftools
 
 vcftools --gzvcf $VCF_file --bed $bed_file --recode --out $guillem/Puffinus_intersect_raw.auto
+vcftools --gzvcf $VCF_file --bed $bed_file2 --recode --out $guillem/Puffinus_intersect_raw.chrZ
